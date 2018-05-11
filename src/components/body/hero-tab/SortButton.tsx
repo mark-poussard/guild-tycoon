795ae9a0576b111e6d2a3fafd6d7f1ced55535dd
Mark <poussard.mark@gmail.com>
2018-05-11 07:28:14 +0100

    [Conf] Trying to figure out font awesome on react

--- src/components/body/hero-tab/SortButton.tsx
+++ src/components/body/hero-tab/SortButton.tsx
@@ -1,24 +1,36 @@
 import * as React from 'react';
-import {SortOrder} from 'model/Sorting';
+import { SortOrder } from 'model/Sorting';
 
-interface ISortButtonProps{
-    order : SortOrder;
-    txt : string;
-    doToggle : () => void;
+interface ISortButtonProps {
+    order: SortOrder;
+    txt: string;
+    doToggle: () => void;
 }
 
-interface ISortButtonState{
+interface ISortButtonState {
 
 }
 
 export default class SortButton extends React.Component<ISortButtonProps, ISortButtonState>{
-    constructor(props:ISortButtonProps){
+    constructor(props: ISortButtonProps) {
         super(props);
     }
 
-    render(){
-        return(
-
+    render() {
+        let icon = null;
+        switch (this.props.order) {
+            case SortOrder.ASC:
+                icon = 'angle-up';
+                break;
+            case SortOrder.DESC:
+                icon = 'angle-down';
+                break
+        }
+        return (
+            <button onClick={this.props.doToggle}>
+                {this.props.txt}
+                <i className={`fa fa-${icon}`} />
+            </button>
         );
     }
 }
